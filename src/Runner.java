38287d6e99592b48c0af017e05ac947cd6212e5e
Vladimir Petrov <vladimir.petrov@lykkex.com>
2018-03-26 11:19:55 +0200

    New part of code to run the algorithm on an external file + updated Runner class to choose between Absolute and Relative price moves.

--- src/Runner.java
+++ src/Runner.java
@@ -6,14 +6,16 @@ public class Runner {
     public double deltaDown;
     public int type;
     public boolean initalized;
+    public boolean absolute;
 
-    public Runner(double threshUp, double threshDown, ATick aTick, int type){
+    public Runner(double threshUp, double threshDown, ATick aTick, int type, boolean absolute){
         extreme = aTick.price;
-        this.type = type; deltaUp = threshUp; deltaDown = threshDown; initalized = true;
+        this.type = type; deltaUp = threshUp; deltaDown = threshDown; initalized = true; this.absolute = absolute;
     }
 
 
     public int run(ATick aTick){
+
         if( aTick == null )
             return 0;
 
@@ -23,6 +25,17 @@ public class Runner {
             return 0;
         }
 
+        if (absolute){
+            return absoluteRun(aTick);
+        } else {
+            return relativeRun(aTick);
+        }
+
+
+    }
+
+
+    private int absoluteRun(ATick aTick){
         if( type == -1 ){
             if( (aTick.price - extreme) >= deltaUp ){
                 type = 1;
@@ -35,6 +48,32 @@ public class Runner {
             }
         }else if( type == 1 ){
             if( (aTick.price - extreme) <= -deltaDown ){
+                type = -1;
+                extreme = aTick.price;
+                return -1;
+            }
+            if( aTick.price > extreme ){
+                extreme = aTick.price;
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    private int relativeRun(ATick aTick){
+        if( type == -1 ){
+            if( (aTick.price - extreme) / extreme >= deltaUp ){
+                type = 1;
+                extreme = aTick.price;
+                return 1;
+            }
+            if( aTick.price < extreme ){
+                extreme = aTick.price;
+                return 0;
+            }
+        }else if( type == 1 ){
+            if( (aTick.price - extreme) / aTick.price <= -deltaDown ){
                 type = -1;
                 extreme = aTick.price;
                 return -1;
